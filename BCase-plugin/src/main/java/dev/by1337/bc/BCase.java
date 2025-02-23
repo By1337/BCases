741489e7a9ba94afca3f1e91c19cf75bd801ad4f
By1337 <by133700@gmail.com>
2025-02-23 15:34:02 +0300

    init

---
+++ BCase-plugin/src/main/java/dev/by1337/bc/BCase.java
@@ -0,0 +1,21 @@
+package dev.by1337.bc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BCase extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
